7f7944aaba5e20472946ca0e6874a5aa2b651584
hunorkelemen <hunorkelemen52@gmail.com>
2023-12-07 10:23:20 +0200

    link modified

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -44,7 +44,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href="https://hunorkelemen.github.io/Login.Register/">
+                <a href="https://hunorkelemen.github.io/Login.Register">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href="https://github.com/hunorkelemen/Login.Register">
